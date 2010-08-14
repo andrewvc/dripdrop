367498faa76c14278f567778fa359fd241de0a5f
Andrew Cholakian <andrew@andrewvc.com>
2010-08-14 12:40:31 -0700

    Reworked lots of the basic structure

--- example/agent_test.rb
+++ example/agent_test.rb
@@ -4,7 +4,7 @@ require '../lib/dripdrop/message'
 agent = DripDrop::Agent.new('tcp://127.0.0.1:2900')
 
 loop do
-  agent.send_message('test','tmsg')
+  agent.send_message('test', :body => 'hello')
   puts "SEND #{agent.address}"
   sleep 1
 end
---
+++ lib/dripdrop/mlogger.rb
@@ -0,0 +1,37 @@
+require 'rubygems'
+require 'ffi-rzmq'
+require 'zmqmachine'
+require 'uri'
+require 'mongo'
+require 'dripdrop/collector'
+
+class DripDrop
+  class MLoggerCollector < Collector
+    attr_accessor :mongo_collection
+    
+    def on_recv(message)
+      if @mongo_collection
+        @mongo_collection.insert(message.to_hash)
+      end
+    end
+  end
+
+  class MLogger
+    attr_reader :sub_address, :sub_reactor, :mongo_host, :mongo_port, :mongo_db,
+                :mongo_connection, :mongo_collection
+
+    def initialize(sub_address='tcp://127.0.0.1:2901',mhost='127.0.0.1',mport=27017,mdb='dripdrop')
+      @sub_address   = URI.parse(sub_address)
+      @sub_collector = MLoggerCollector.new('tcp://127.0.0.1:2901')
+      
+      @mongo_host, @mongo_port, @mongo_db = mhost, mport, mdb
+      @mongo_connection = Mongo::Connection.new(@mongo_host,@mongo_port).db(@mongo_db)
+      @mongo_collection = @mongo_connection.collection('raw')
+    end
+
+    def run
+      @sub_collector.mongo_collection = @mongo_collection
+      @sub_collector.run.join
+    end
+  end
+end
