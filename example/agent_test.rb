a0c296f3bb4950ded6fb10b83675793e1a40236c
Andrew Cholakian <andrew@andrewvc.com>
2010-08-23 22:24:17 -0700

    1.8.7 Agent support

--- example/agent_test.rb
+++ example/agent_test.rb
@@ -7,7 +7,7 @@ loop do
   #Test is the message name, this is the first part of the 0MQ message, used for filtering
   #at the 0MQ sub socket level, :head is always a hash,  :body is freeform
   #EVERYTHING must be serializable to BERT
-u agent.send_message('test', :body => 'hello', :head => {:key => 'value'})
+  agent.send_message('test', :body => 'hello', :head => {:key => 'value'})
   puts "SEND"
   sleep 1
 end
