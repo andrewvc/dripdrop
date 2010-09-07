ae50c273dcf3af93ce9c14a757971093f9b7a22a
Andrew Cholakian <andrew@andrewvc.com>
2010-09-06 22:47:28 -0700

    HTTP Client support

--- example/http.rb
+++ example/http.rb
@@ -10,15 +10,13 @@ DripDrop::Node.new do |node|
     response.send_message(msg)
   end
 
-  #Looks like em's http client can't send bodies in requests...
-  #once I fix this, this will work...
-  #EM::PeriodicTimer.new(1) do
-  #  client = node.http_client(addr)
-  #  msg = DripDrop::Message.new('http/status', :body => "Success #{i}")
-  #  client.send_message(msg) do |resp_msg|
-  #    puts resp_msg.inspect
-  #  end
-  #end
+  EM::PeriodicTimer.new(1) do
+    client = node.http_client(addr)
+    msg = DripDrop::Message.new('http/status', :body => "Success #{i}")
+    client.send_message(msg) do |resp_msg|
+      puts resp_msg.inspect
+    end
+  end
 
   #Keep zmqmachine from spinning around using up all our CPU by creating a socket
   req = node.zmq_xreq('tcp://127.0.0.1:2091', :connect)
