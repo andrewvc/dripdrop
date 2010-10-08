d13ac5ce09ecbb955b352f361733861fb1ca2f4f
Andrew Cholakian <andrew@andrewvc.com>
2010-10-08 11:00:09 -0400

    Updated examples to use the new cleaner syntax

--- example/http.rb
+++ example/http.rb
@@ -1,17 +1,17 @@
 require 'dripdrop/node'
 Thread.abort_on_exception = true
 
-DripDrop::Node.new do |node|
+DripDrop::Node.new do
   addr = 'http://127.0.0.1:2200'
   
   i = 0 
-  node.http_server(addr).on_recv do |response,msg|
+  http_server(addr).on_recv do |response,msg|
     i += 1
     response.send_message(msg)
   end
 
   EM::PeriodicTimer.new(1) do
-    client = node.http_client(addr)
+    client = http_client(addr)
     msg = DripDrop::Message.new('http/status', :body => "Success #{i}")
     client.send_message(msg) do |resp_msg|
       puts resp_msg.inspect
@@ -19,5 +19,5 @@ DripDrop::Node.new do |node|
   end
 
   #Keep zmqmachine from spinning around using up all our CPU by creating a socket
-  req = node.zmq_xreq('tcp://127.0.0.1:2091', :connect)
-end
+  req = zmq_xreq('tcp://127.0.0.1:2091', :connect)
+end.start!
