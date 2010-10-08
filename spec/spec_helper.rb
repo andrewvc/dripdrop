832cfd874197f4eb6fae8a279b5e2921c5ba5bbc
Andrew Cholakian <andrew@andrewvc.com>
2010-10-08 16:20:47 -0400

    Initial xrep/xreq spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require File.expand_path(File.join(File.dirname(__FILE__), %w[.. lib dripdrop]))
+Thread.abort_on_exception = true
 
 def rand_addr
-  "tcp://127.0.0.1:#{rand(10_1000) + 20_000}"
+  "tcp://127.0.0.1:#{rand(10_000) + 20_000}"
 end
