270df546e5fe7b3b3ebb64642ab3fe6f4d0f68e5
Andrew Cholakian <andrew@andrewvc.com>
2010-10-21 20:25:05 -0700

    Add todo note to agent test

--- example/agent_test.rb
+++ example/agent_test.rb
@@ -1,12 +1,13 @@
+##
+##TODO: This badly needs to be rewritten
+##
+
 require 'rubygems'
 require 'dripdrop/agent'
 
 agent = DripDrop::Agent.new(ZMQ::PUB,'tcp://127.0.0.1:2900',:connect)
 
 loop do
-  #Test is the message name, this is the first part of the 0MQ message, used for filtering
-  #at the 0MQ sub socket level, :head is always a hash,  :body is freeform
-  #EVERYTHING must be serializable to BERT
   agent.send_message('test', :body => 'hello', :head => {:key => 'value'})
   puts "SEND"
   sleep 1
